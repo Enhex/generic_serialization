2e81981f0cc815dbc89ddb87a03d3c1cfd113c5b
Enhex <enhex0@gmail.com>
2018-11-16 12:50:31 +0200

    updated conan

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -7,8 +7,9 @@ class GenericserializationTestConan(ConanFile):
 
     def build(self):
         cmake = CMake(self)
-        # Current dir is "test_package/build/<build_id>" and CMakeLists.txt is in "test_package"
-        cmake.configure(source_dir=self.conanfile_directory, build_dir="./")
+        # Current dir is "test_package/build/<build_id>" and CMakeLists.txt is
+        # in "test_package"
+        cmake.configure()
         cmake.build()
 
     def imports(self):
